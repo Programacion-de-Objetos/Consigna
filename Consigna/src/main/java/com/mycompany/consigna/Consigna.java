86210feeb68cc7d925713919c06ead9b19bdb23c
tebankai <87548616+tebankai@users.noreply.github.com>
2024-10-06 17:55:52 -0600

    Add files via upload

---
+++ Consigna/src/main/java/com/mycompany/consigna/Consigna.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.consigna;
+
+/**
+ *
+ * @author ester
+ */
+public class Consigna {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
